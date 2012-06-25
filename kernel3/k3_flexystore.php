23fa252de2572e35d705abef6759b85b1f6e4045
Foxel <foxel@quickfox.ru>
2012-06-25 10:54:57 +0700

    Added GPLv3 notes

--- kernel3/k3_flexystore.php
+++ kernel3/k3_flexystore.php
@@ -1,5 +1,25 @@
 <?php
 /**
+ * Copyright (C) 2011 Andrey F. Kupreychik (Foxel)
+ *
+ * This file is part of QuickFox Kernel 3.
+ * See https://github.com/foxel/Kernel3/ for more details.
+ *
+ * Kernel 3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kernel 3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kernel 3. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * QuickFox kernel 3 'SlyFox' Flexible data storage table class
  * Requires PHP >= 5.1.0
  * @package kernel3
