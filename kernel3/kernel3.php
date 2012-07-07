b37212c36ec0d976b48b8ebe8aea21fc37bd8eb6
Foxel <foxel@quickfox.ru>
2012-07-07 13:29:17 +0700

    Improved debug

--- kernel3/kernel3.php
+++ kernel3/kernel3.php
@@ -95,8 +95,9 @@ if (get_magic_quotes_runtime()) {
 set_time_limit(30);  // not '0' - once i had my script running for a couple of hours collecting GBytes of errors :)
 // here comes the fatal catcher :P
 register_shutdown_function(create_function('', 'if (($a = error_get_last()) && $a[\'type\'] == E_ERROR)
-    { file_put_contents(F_LOGS_ROOT.DIRECTORY_SEPARATOR.\'php_fatal.log\', sprintf(\'E%d "%s" at %s:%d\', $a[\'type\'], $a[\'message\'], $a[\'file\'], $a[\'line\']));
-    $i = ob_get_level(); while ($i--) @ob_end_clean(); print \'Fatal error. Sorry :(\'; }'));
+    { $message = sprintf(\'E%d "%s" at %s:%d\', $a[\'type\'], $a[\'message\'], $a[\'file\'], $a[\'line\']);
+    file_put_contents(F_LOGS_ROOT.DIRECTORY_SEPARATOR.\'php_fatal.log\', $message);
+    $i = ob_get_level(); while ($i--) @ob_end_clean(); print F_DEBUG ? $message : \'Fatal error. Sorry :(\'; }'));
 
 // here we set an error catcher
 set_error_handler(create_function('$c, $m, $f, $l', 'throw new ErrorException($m, 0, $c, $f, $l);'),
