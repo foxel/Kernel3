7ca9fbc28fe2b1be8bd3bb7370ec59316c0927ac
Foxel <foxel@quickfox.ru>
2012-08-29 22:50:51 +0700

    Improve XSS filtering

---
+++ kernel3/K3/DOM.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright (C) 2012 Andrey F. Kupreychik (Foxel)
+ *
+ * This file is part of QuickFox Kernel 3.
+ * See https://github.com/foxel/Kernel3/ for more details.
+ *
+ * Kernel 3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kernel 3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kernel 3. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class K3_DOM extends DOMDocument
+{
+    /**
+     * @return K3_DOM
+     */
+    public function stripXSSVulnerableCode()
+    {
+        $xpath = new DOMXPath($this);
+        // Register the php: namespace
+        $xpath->registerNamespace('php', 'http://php.net/xpath');
+        // Register PHP function
+        $xpath->registerPHPFunctions('stripos');
+
+        $forbiddenNodes = $xpath->query('//script|//iframe|//frame');
+        foreach ($forbiddenNodes as $item) {
+            /** @var $item DOMElement */
+            $item->parentNode->removeChild($item);
+        }
+
+        $forbiddenAttributes = array(
+            'onload', 'onunload', 'onabort',
+            'onblur', 'onchange', 'onfocus', 'onreset', 'onselect', 'onsubmit',
+            'onkeydown', 'onkeypress', 'onkeyup',
+            'onclick', 'ondblclick', 'onmousedown', 'onmouseup', 'onmousemove', 'onmouseout', 'onmouseover',
+            'href' => 'javascript:'
+        );
+        foreach ($forbiddenAttributes as $key => $value) {
+            if (!is_int($key)) {
+                $selector      = "//*[php:functionString('stripos', @$key, '$value')]";
+                $attributeName = $key;
+            } else {
+                $selector      = "//*[@$value]";
+                $attributeName = $value;
+            }
+            $items = $xpath->query($selector);
+            foreach ($items as $item) {
+                /** @var $item DOMElement */
+                $item->removeAttribute($attributeName);
+            }
+        }
+
+        return $this;
+    }
+}
