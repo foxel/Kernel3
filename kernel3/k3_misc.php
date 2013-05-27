21ac548cfe9ad43772378ac2a098791341ef2fd3
Foxel <foxel@quickfox.ru>
2013-05-27 14:39:23 +0700

    simple scalar comparator

--- kernel3/k3_misc.php
+++ kernel3/k3_misc.php
@@ -544,6 +544,21 @@ final class FMisc
 
         return $data;
     }
+
+    /**
+     * simple scalar compare
+     *
+     * @param mixed $a
+     * @param mixed $b
+     * @return int
+     */
+    static public function scalarCmp($a, $b)
+    {
+        if ($a == $b) {
+            return 0;
+        }
+        return ($a < $b) ? -1 : 1;
+    }
 }
 
 FMisc::initCore();
