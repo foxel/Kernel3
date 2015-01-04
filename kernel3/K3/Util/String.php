89561d48182d83b7b8de5448f4e3f814a79454a5
Foxel <foxel@quickfox.ru>
2015-01-04 12:13:03 +0600

    Year fixes

--- kernel3/K3/Util/String.php
+++ kernel3/K3/Util/String.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (C) 2014 Andrey F. Kupreychik (Foxel)
+ * Copyright (C) 2014 - 2015 Andrey F. Kupreychik (Foxel)
  *
  * This file is part of QuickFox Kernel 3.
  * See https://github.com/foxel/Kernel3/ for more details.
